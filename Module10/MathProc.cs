f1ef028b19a5d5093765e17f9e797138bb55f7f2
PhoenixOleg <metall_2000@mail.ru>
2023-10-13 23:00:25 +0300

    Комит Задания 1

---
+++ Module10/MathProc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    /// <summary>
+    /// Класс арифметических операций, наследующий интерфейс IMathProc
+    /// </summary>
+    public class MathProc : IMathProc
+    {
+        /// <summary>
+        /// Метод нахождения суммы, который не был реализован по умолчанию в интерфейсе
+        /// </summary>
+        /// <param name="a">>Слагаемое 1</param>
+        /// <param name="b">>Слагаемое 2</param>
+        /// <returns></returns>
+        double IMathProc.SumImplementItYourself(double a, double b) { return a + b; } //=> Нереализованный по-умолчанию метод интерфейса надо реализовать самому
+    }
+}
